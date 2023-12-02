b27ab16f354dd36430a8a0d0094fc62dd213a861
saadERRAOUI <saaderraouiinfosup@gmail.com>
2023-12-02 21:46:16 +0100

    PUSH-GL: Update Header files

--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,25 @@
 /*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 23:53:44 by serraoui          #+#    #+#             */
-/*   Updated: 2023/12/02 20:57:56 by serraoui         ###   ########.fr       */
+/*   Updated: 2023/12/02 21:45:47 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+# if BUFFER_SIZE > 2147483647
+#  define BUFFER_SIZE 0
+# endif
+
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 32
-#endif
-
-#if BUFFER_SIZE > 2147483647
-# define BUFFER_SIZE 0
-#endif
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
